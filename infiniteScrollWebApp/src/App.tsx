fcc23a07b3110606fc91e62c12896f2a3f791025
August <76185140+Augustina1995@users.noreply.github.com>
2024-08-08 11:57:15 +0300

    set up css styling for the page

--- infiniteScrollWebApp/src/App.tsx
+++ infiniteScrollWebApp/src/App.tsx
@@ -1,14 +1,12 @@
-
-
+import GalleryPage from "./pages/GalleryPage/GalleryPage";
+import "./styles/global.scss";
 
 function App() {
-
-
   return (
     <>
-     
+      <GalleryPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
